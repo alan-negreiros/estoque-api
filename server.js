b49b5e6f6313565c7efe8d56c06f397798c7c056
Alan Cauê <alancauenegreiros@gmail.com>
2026-08-03 02:02:51 -0300

    feat: integra endpoint de status do estoque

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoutes = require('./src/routes/userRoutes');
+const estoqueRoutes = require('./src/routes/estoqueRoutes');
 const app = express();
 
 app.use(express.json());
 app.use('/api/usuarios', userRoutes);
+app.use('/api/estoque', estoqueRoutes);
 
 app.listen(3000, () => console.log('API rodando na porta 3000'));
