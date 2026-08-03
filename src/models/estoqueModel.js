85cd1d58cbeb7ae9e9f15a021a3647619362c3ce
Alan Cauê <alancauenegreiros@gmail.com>
2026-08-03 02:00:36 -0300

    feat: cria model de estoque lendo regras do submodulo

---
+++ src/models/estoqueModel.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+// Consumindo o Submodulo 1
+const regrasPath = path.join(__dirname, '../../contracts/regras_produtos.json');
+const regras = JSON.parse(fs.readFileSync(regrasPath, 'utf8'));
+
+let inventario = [
+    { sku: "PROD-1", qtd: 15 }
+];
+
+module.exports = {
+    obterEstoque: () => inventario,
+    verificarAlerta: (qtdAtual) => qtdAtual <= regras.alerta_estoque_baixo
+};
